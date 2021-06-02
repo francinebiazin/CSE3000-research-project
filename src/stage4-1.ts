fa6d020a4d814da936a2821280143d59df67a4ab
Francine Biazin <francine.biazin@gmail.com>
2021-06-03 00:42:52 +0200

    Fix list of subpages to request

--- src/stage4-1.ts
+++ src/stage4-1.ts
@@ -48,38 +48,19 @@ fs.mkdir(csvDir, { recursive: true }, (error) => {
   if (error) throw error
 })
 
-// load the domains from list created from Stage 4.1 data
-const domainsPath = 'domains/2021-6-1-stage4domains.csv'
+// load the domains from list created from Stage 3 data
+const domainsPath = 'domains/2021-5-22-stage4domains.csv'
 const parser = papaparse.parse(fs.readFileSync(domainsPath, { encoding: 'utf-8' }))
-const links: string[][] = []
-let pages: string[] = []
-let dom = 'none'
-for (let i = 0; i < parser.data.length; i++) {
-  const row: any = parser.data[i]
-  if (typeof row[1] == 'undefined') {
-    continue
-  }
-  // new domain
-  if (row[0] == '0') {
-    // push array of subpages to array of links
-    if (pages.length > 0) {
-      links.push([...pages])
-    }
-    // reset array of subpages
-    pages = []
-    const link = row[1]
-    dom = link.split("//")[1]
-    pages.push(link)
-  }
-  else {
-    const link = row[1]
-    if (link.includes(dom)) {
-      pages.push(link)
-    }
-  }
+const domains: string[] = []
+let l = 0
+let added = 0
+while (added < numberDomains) {
+  const row : any = parser.data[l]
+  const domain = row[1]
+  domains.push(domain)
+  l++
+  added++
 }
-// push final set of pages to links array
-links.push([...pages])
 
 // prepare the CSV file writer
 const createCsvWriter = require('csv-writer').createObjectCsvWriter
@@ -93,6 +74,7 @@ const csvWriter = createCsvWriter({
     {id: 'attempts', title: 'Attempts'},
     {id: 'ogdomain', title: 'Request Domain'},
     {id: 'resdomain', title: 'Response Domain'},
+    {id: 'links', title: 'Links Found'},
     {id: 'ip', title: 'IP Address'},
     {id: 'status', title: 'HTTP Status Code'},
     {id: 'error', title: 'Error'},
@@ -143,47 +125,91 @@ async function runBrowser() {
       page.removeAllListeners('requests')
       // time request
       const startTime = process.hrtime.bigint()
-      // get home and subpages
-      const pages = links[j]
-      // get domain
-      const domain = pages[0].split("//")[1]
-      for (let g = 0; g < pages.length; g++) {
-        // retry non-2xx requests up to 3 times
-        for (let i = 1; i < requestRetries+1; i++) {
-          try {
-            const domainResponse = await page.goto(pages[g], { waitUntil: 'domcontentloaded', timeout: timeouts[i] })
-            // let extensions do their work
-            await page.waitForTimeout(6000)
-            let statusCode = domainResponse?.status()
-            // check empty response
-            if (typeof statusCode == 'undefined') {
-              // special value so a screenshot will be taken, but we know something was not quite right
-              statusCode = 215
-            }
-            // take screenshot if status code 2xx
-            if (typeof statusCode !== 'undefined' && statusCode > 199 && statusCode < 300) {
-              const screenshotPath = screenshotDir + '/' + fullDate + '-' + index + '-' + domain + '-' + g + '.png'
-              await page.screenshot({ path: screenshotPath })
-            }
-            await saveData(startTime, g, i, pages[g], page.url(), ipAddress, statusCode, 'none')
-            break
-          } catch (error) {
-            if (i == requestRetries) {
-              await saveData(startTime, g, i, pages[g], 'none', ipAddress, 0, error.message)
-            }
-            else {
-              await page.waitForTimeout(5000)
-            }
+      // get complete domain path
+      const completeDomain = 'http://' + domains[j]
+      // links for subpages
+      let links: string[] = []
+      // retry non-2xx requests up to 3 times
+      for (let i = 1; i < requestRetries+1; i++) {
+        try {
+          // reset links
+          links = []
+          const domainResponse = await page.goto(completeDomain, { waitUntil: 'domcontentloaded', timeout: timeouts[i] })
+          // let extensions do their work
+          await page.waitForTimeout(6000)
+          let statusCode = domainResponse?.status()
+          // check empty response
+          if (typeof statusCode == 'undefined') {
+            // special value so a screenshot will be taken, but we know something was not quite right
+            statusCode = 215
+          }
+          // take screenshot if status code 2xx
+          if (typeof statusCode !== 'undefined' && statusCode > 199 && statusCode < 300) {
+            const screenshotPath = screenshotDir + '/' + fullDate + '-' + index + '-' + domains[j] + '-0.png'
+            await page.screenshot({ path: screenshotPath })
+            // get links
+            const hrefs = await page.evaluate(() => 
+              Array.from(document.querySelectorAll("a")).map(anchor => anchor.href)
+            )
+            links = hrefs.filter(link => (link.includes(domains[j] + '/') && link !== page.url() && link !== (page.url() + '/#')))
+            // links.map(link => console.log(link))
+          }
+          await saveData(startTime, 0, i, completeDomain, page.url(), links.length, ipAddress, statusCode, 'none')
+          break
+        } catch (error) {
+          if (i == requestRetries) {
+            await saveData(startTime, 0, i, completeDomain, 'none', links.length, ipAddress, 0, error.message)
+          }
+          else {
+            await page.waitForTimeout(5000)
           }
         }
-        index++
       }
+      index++
       domainIndex++
+      // request subpages
+      if (links.length > 0) {
+        // request 2 subpages
+        for (let k = 0; k < 2; k++) {
+          // time request
+          const startTime = process.hrtime.bigint()
+          // retry non-2xx requests up to 3 times
+          for (let i = 1; i < requestRetries+1; i++) {
+            try {
+              const domainResponse = await page.goto(links[k], { waitUntil: 'domcontentloaded', timeout: timeouts[i] })
+              // let extensions do their work
+              await page.waitForTimeout(6000)
+              let statusCode = domainResponse?.status()
+              // check empty response
+              if (typeof statusCode == 'undefined') {
+                // special value so a screenshot will be taken, but we know something was not quite right
+                statusCode = 215
+              }
+              // take screenshot if status code 2xx
+              if (typeof statusCode !== 'undefined' && statusCode > 199 && statusCode < 300) {
+                const screenshotPath = screenshotDir + '/' + fullDate + '-' + index + '-' + domains[j] + '-' + (k+1) + '.png'
+                await page.screenshot({ path: screenshotPath })
+              }
+              await saveData(startTime, k+1, i, links[k], page.url(), 0, ipAddress, statusCode, 'none')
+              break
+            } catch (error) {
+              if (i == requestRetries) {
+                await saveData(startTime, k+1, i, links[k], page.url(), 0, ipAddress, 0, error.message)
+              }
+              else {
+                await page.waitForTimeout(3000)
+              }
+            }
+          }
+          index++
+        }
+      }
       // clear cache & cookies every clearoutLimit requests
       client = await page.target().createCDPSession()
       await client.send('Network.clearBrowserCookies')
       await client.send('Network.clearBrowserCache')
       // await page.waitForTimeout(1000)
+      // page.removeAllListeners()
       await page.close()
     }
 
@@ -193,7 +219,7 @@ async function runBrowser() {
   })
 }
 
-async function saveData(start: bigint, subpageIndex: number, attemptNumber: number, ogDomain: string, resDomain: string, ipAddress: string | undefined, statusCode: number, error: string) {
+async function saveData(start: bigint, subpageIndex: number, attemptNumber: number, ogDomain: string, resDomain: string, totalLinks: number, ipAddress: string | undefined, statusCode: number, error: string) {
   // sort out timestamp for request
   datetime = new Date()
   const hours = datetime.getHours()
@@ -208,6 +234,7 @@ async function saveData(start: bigint, subpageIndex: number, attemptNumber: numb
     'attempts': attemptNumber,
     'ogdomain': ogDomain,
     'resdomain': resDomain,
+    'links': totalLinks,
     'ip': ipAddress, 
     'status': statusCode, 
     'error': error
